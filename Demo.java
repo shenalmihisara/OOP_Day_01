ec858717473c9b50f1d9bdafac4d9d18978584c2
shenalmihisara <shenalmihisara51@gmail.com>
2026-07-17 16:03:27 +0530

    Refactor Box class constructors for improved initialization; update Demo class to demonstrate new constructor functionality

--- Demo.java
+++ Demo.java
@@ -190,29 +190,26 @@ class Box{
 	int length;
 	int width;
 	int height;
-	
-	//-----constroctur-------
 
-	/*Box(){
-		length=0;
-		width=0;
-		height=0;
-	}*/
+	Box(){
+		length=1;
+		width=1;
+		height=1;
+		System.out.println("Box()");
+	}
 	
-	//------default constructor-----------
-	
-	/*Box(){
-		length=10;
-		width=20;
-		height=30;
-	}*/
-	
-	//------parametarized constroctor----------
-	
+	Box(int length){
+		this.length=length;
+		width=length;
+		height=length;
+		System.out.println("Box(int int int)");
+	}
+
 	Box(int length, int width, int height){
 		this.length = length;
 		this.width = width;
 		this.height = height;
+		System.out.println("Box(int)");
 	}
 	
 	public void printVolume(){
@@ -223,11 +220,14 @@ class Box{
 }
 class Demo{
 	public static void main(String args[]){
-		Box b1=new Box(5,10,15);
-		b1.printVolume(); //?
-		System.out.println("Length : "+b1.length);	// ?
-		System.out.println("Width  : "+b1.width); 	// ?
-		System.out.println("Height : "+b1.height);	// ?
+		Box b1=new Box();
+		b1.printVolume();
+		
+		Box b2=new Box(10);
+		b2.printVolume();
+		
+		Box b3=new Box(5,10,15);
+		b3.printVolume();
 	}
 }
 
